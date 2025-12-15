cea07f85a41d74598cd200c228ee4ce71a926054
mustafaacarinfo <62604831+mustafaacarinfo@users.noreply.github.com>
2025-12-15 16:35:37 +0300

    Delete Debug directory

--- Debug/Components/Actuator/Src/actuator_ram.d
+++
@@ -1,6 +0,0 @@
-Components/Actuator/Src/actuator_ram.o: \
- ../Components/Actuator/Src/actuator_ram.c \
- C:/Users/mustafa.acar/STM32CubeIDE/workspace_1.19.0/Actuator_Homing_FW/Components/Actuator/Inc/actuator_ram.h \
- C:/Users/mustafa.acar/STM32CubeIDE/workspace_1.19.0/Actuator_Homing_FW/Components/Actuator/Inc/actuator_types.h
-C:/Users/mustafa.acar/STM32CubeIDE/workspace_1.19.0/Actuator_Homing_FW/Components/Actuator/Inc/actuator_ram.h:
-C:/Users/mustafa.acar/STM32CubeIDE/workspace_1.19.0/Actuator_Homing_FW/Components/Actuator/Inc/actuator_types.h:
